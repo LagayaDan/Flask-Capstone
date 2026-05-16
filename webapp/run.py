030955f5eb763637add192aaa2ce3ce7a24ad9c6
LagayaDan <164594518+LagayaDan@users.noreply.github.com>
2026-05-17 00:02:50 +0800

    Update run.py

--- webapp/run.py
+++ webapp/run.py
@@ -13,4 +13,4 @@ from webapp import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
